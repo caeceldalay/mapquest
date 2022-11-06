586c1b05f5dc3d2e467e6609fbed9698fb64fdaa
caeceldalay <111572992+caeceldalay@users.noreply.github.com>
2022-11-06 15:13:42 +0800

    Update mapquest_parse-json_7.py

--- mapquest_parse-json_7.py
+++ mapquest_parse-json_7.py
@@ -8,7 +8,7 @@ key = "k2BGKwAMKCqQB7KpRhTiC9fPY7OfCZsM"
 now = date.today()
 
 
-name = input(Fore.BLUE + "Please Enter Traveller's Name: ")
+name = input(Fore.RED + "Please Enter Traveller's Name: ")
 print("                                              ")
 print(Fore.RED + "Class 1 Vehicles: CAR \n, Van \n, Pick-Up \n, Motorcycle \n")
 print(Fore.RED +"Class 2 Vehicles: Bus\n, Truck \n")
